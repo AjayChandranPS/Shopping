20b6d3b329874de44a622093fca04e917782f487
Ajay P S <ajaychandranps@gmail.com>
2023-03-01 17:29:12 +0530

    new additions

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
 
+    this.CartDetails();
+
+  }
+
+  getCartDetails: any = [];
+  CartDetails() {
+    if (localStorage.getItem('localCart')) {
+      let tmp: any = localStorage.getItem('localCart');
+      this.getCartDetails = JSON.parse(tmp);
+
+      console.log(tmp);
+
+    }
+  }
+
+  singleDelete(getCartDetail) {
+    console.log(getCartDetail);
+    if (localStorage.getItem('localCart')) {
+      let tmp:any= localStorage.getItem('localCart');
+      this.getCartDetails = JSON.parse(tmp);
+      for (let i = 0; i < this.getCartDetails.length; i++) {
+        if (this.getCartDetails[i].prodId === getCartDetail) {
+          console.log(getCartDetail);
+          this.getCartDetails.splice(i, 1);
+          localStorage.setItem('localCart', JSON.stringify(this.getCartDetails));
+        }
+      }
+    }
+  }
 }
